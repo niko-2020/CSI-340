fed13da7170427f3399ac3a3c29df4c299e755d7
Alex Chen <alche@microsoft.com>
2022-01-22 09:25:42 -0800

    created table models

---
+++ CSI-340_ChenRoblesWu_Website/CSI-340_ChenRoblesWu_Website/Models/BookModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSI_340_ChenRoblesWu_Website.Models
+{
+    public class BookModel
+    {
+        public string BookName { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ CSI-340_ChenRoblesWu_Website/CSI-340_ChenRoblesWu_Website/Models/PriceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSI_340_ChenRoblesWu_Website.Models
+{
+    public class PriceModel
+    {
+        public float Price { get; set; }
+
+        public int PriceId { get; set; }
+    }
+}
