e34349478eb915064f1a7e89efc0e636f8070d72
Mengni Wu <nikowu@vrv.earth>
2022-02-08 11:32:39 -0800

    Add Inventory and Update Inventorycontroll

---
+++ CSI-340_ChenRoblesWu_Website/CSI-340_ChenRoblesWu_Website/Controllers/InventoryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CSI_340_ChenRoblesWu_Website.Data;
+using CSI_340_ChenRoblesWu_Website.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CSI_340_ChenRoblesWu_Website.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private readonly DataContext _db;
+
+        public InventoryController(DataContext db)
+        {
+            _db = db;
+        }
+        public IActionResult BrowseBooks()
+        {
+            var displayData = _db.Book.ToList();
+            return View("BrowseBooks",displayData);
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Login(int Customer_id)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(Customer_id);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("ContactInfo");
+            }
+            return View(Customer_id);
+        }
+
+    }
+}
+
