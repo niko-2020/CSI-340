97fcd53d1dcad9fa73b7557ee76db0688bd1aaab
Mengni Wu <nikowu@vrv.earth>
2022-02-07 19:37:08 -0800

    Create Login cshtml

--- CSI-340_ChenRoblesWu_Website/CSI-340_ChenRoblesWu_Website/Models/CustomerModel.cs
+++ CSI-340_ChenRoblesWu_Website/CSI-340_ChenRoblesWu_Website/Models/CustomerModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,7 @@ namespace CSI_340_ChenRoblesWu_Website.Models
 {
     public class CustomerModel
     {
-        public int Id { get; set; }
-
+        [Key]
         public int CustomerId { get; set; }
         public string FirstName { get; set; }
 
