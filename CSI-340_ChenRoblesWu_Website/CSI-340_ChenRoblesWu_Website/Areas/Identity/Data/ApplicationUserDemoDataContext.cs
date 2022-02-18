e034ba9006699699ea2d109faf6f4342e90dc8b4
Alex Chen <alche@microsoft.com>
2022-02-17 21:48:28 -0800

    stupid registration page

---
+++ CSI-340_ChenRoblesWu_Website/CSI-340_ChenRoblesWu_Website/Areas/Identity/Data/ApplicationUserDemoDataContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSI_340_ChenRoblesWu_Website.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CSI_340_ChenRoblesWu_Website.Areas.Identity.Data
+{
+    public class ApplicationUserDemoDataContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationUserDemoDataContext(DbContextOptions<ApplicationUserDemoDataContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+        }
+    }
+
+    public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder) 
+        {
+            builder.Property(u => u.FirstName).HasMaxLength(255);
+            builder.Property(u => u.LastName).HasMaxLength(255);
+        }
+    }
+}
---
+++ CSI-340_ChenRoblesWu_Website/CSI-340_ChenRoblesWu_Website/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ApplicationUserDemo.Areas.Identity;
+using CSI_340_ChenRoblesWu_Website.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ApplicationUserDemo.Areas.Identity.IdentityHostingStartup))]
+namespace ApplicationUserDemo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ApplicationUserDemoDataContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationUserDemoDataContext>();
+            });
+        }
+    }
+}
